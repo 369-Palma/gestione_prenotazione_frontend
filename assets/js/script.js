02a9ea6cd8b6ec3f8e3d991e46f0713e6565c6c1
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-09-22 14:59:20 +0200

    work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,6 @@ async function getPostazioni() {
        <th> <strong>Edificio</strong> </th>
        <th> <strong>Indirizzo</strong> </th>
        <th> <strong>Città</strong> </th>
-      
     </tr>
     `;
 
@@ -59,6 +58,37 @@ async function getPostazioni() {
           tableElement.appendChild(trElement);
         });
       }
+
+      const ulElement = document.getElementById("lista");
+
+      // Itera attraverso i dati e crea gli elementi <li> con le informazioni richieste
+      data.content.forEach((item) => {
+        const liElement = document.createElement("li");
+
+        // Creazione della riga con le informazioni
+        const row = document.createElement("div");
+        row.innerHTML = `
+        <div class="listaRis">
+        <div>
+        <strong>ID:</strong> ${item.id}<br>
+        <strong>Codice:</strong> ${item.codice}<br>
+        <strong>Descrizione:</strong> ${item.descrizione}<br>
+        <strong>Numero Max Occupanti:</strong> ${item.numeroMaxOccupanti}<br>
+        <strong>Disponibile:</strong> ${item.available ? "Sì" : "No"}<br>
+        <strong>Tipo:</strong> ${item.tipo}<br>
+        <strong>Edificio:</strong> ${item.building.name}<br>
+        <strong>Indirizzo:</strong> ${item.building.address}<br>
+        <strong>Città:</strong> ${item.building.citta.name}<br>
+        </div>
+        <button> Prenota </button>
+        </div>
+      `;
+
+        // Aggiungi la riga al <li>
+        liElement.appendChild(row);
+
+        ulElement.appendChild(liElement);
+      });
     }
   } catch (error) {
     console.error(error);
