13fe789f59960d6ee17709571f5b1db4523541e2
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-09-29 09:56:53 +0200

    cerca per città e tipo di postazione

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
-const handleSubmit = () => {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
   getPostazioni();
-  return "non ci sono postazioni";
-};
+
+  document
+    .getElementById("cercaForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const formData = new FormData(event.target);
+      const citta = formData.get("name");
+      const tipo = formData.get("tipo");
+      console.log("Il tipo selezionato è: ", tipo);
+
+      if (!tipo || tipo === "tipo") {
+        alert("Seleziona un tipo di postazione valido");
+        return;
+      }
+
+      getPostazioniDisponibili(citta, tipo);
+    });
+});
+
+const tableElement = document.getElementById("risultatiLista");
+const ulElement = document.getElementById("lista");
 
 async function getPostazioni() {
   try {
@@ -11,7 +30,7 @@ async function getPostazioni() {
       throw new Error("Errore nella richiesta API");
     } else {
       let data = await res.json();
-      console.log("data: ", data);
+      console.log("data: ", data.content);
 
       let tableElement = document.getElementById("risultatiLista");
 
@@ -27,33 +46,33 @@ async function getPostazioni() {
         const theadElement = document.createElement("thead");
 
         theadElement.innerHTML = `
-    <tr>
-       <th> <strong>Codice</strong> </th>
-       <th> <strong>Descrizione</strong> </th>
-       <th> <strong>N° Max</strong> </th>
-       <th> <strong>Disponibile</strong> </th>
-       <th> <strong>Tipo</strong> </th>
-       <th> <strong>Edificio</strong> </th>
-       <th> <strong>Indirizzo</strong> </th>
-       <th> <strong>Città</strong> </th>
-    </tr>
-    `;
+      <tr>
+         <th> <strong>Codice</strong> </th>
+         <th> <strong>Descrizione</strong> </th>
+         <th> <strong>N° Max</strong> </th>
+         <th> <strong>Disponibile</strong> </th>
+         <th> <strong>Tipo</strong> </th>
+         <th> <strong>Edificio</strong> </th>
+         <th> <strong>Indirizzo</strong> </th>
+         <th> <strong>Città</strong> </th>
+      </tr>
+      `;
 
         tableElement.appendChild(theadElement);
 
         data.content.forEach((item) => {
           const trElement = document.createElement("tr");
           trElement.innerHTML = `
-          <td>${item.codice}</td>
-          <td>${item.descrizione}</td>
-          <td>${item.numeroMaxOccupanti}</td>
-          <td>${item.available ? "Sì" : "No"}</td>
-          <td>${item.tipo}</td>
-          <td>${item.building.name}</td>
-          <td>${item.building.address}</td>
-          <td>${item.building.citta.name}</td>
-          <td> <button> Prenota </button></td>
-        `;
+            <td>${item.codice}</td>
+            <td>${item.descrizione}</td>
+            <td>${item.numeroMaxOccupanti}</td>
+            <td>${item.available ? "Sì" : "No"}</td>
+            <td>${item.tipo}</td>
+            <td>${item.building.name}</td>
+            <td>${item.building.address}</td>
+            <td>${item.building.citta.name}</td>
+            <td> <button> Prenota </button></td>
+          `;
 
           tableElement.appendChild(trElement);
         });
@@ -68,23 +87,114 @@ async function getPostazioni() {
         // Creazione della riga con le informazioni
         const row = document.createElement("div");
         row.innerHTML = `
-        <div class="listaRis">
-        <div>
-        <strong>ID:</strong> ${item.id}<br>
-        <strong>Codice:</strong> ${item.codice}<br>
-        <strong>Descrizione:</strong> ${item.descrizione}<br>
-        <strong>Numero Max Occupanti:</strong> ${item.numeroMaxOccupanti}<br>
-        <strong>Disponibile:</strong> ${item.available ? "Sì" : "No"}<br>
-        <strong>Tipo:</strong> ${item.tipo}<br>
-        <strong>Edificio:</strong> ${item.building.name}<br>
-        <strong>Indirizzo:</strong> ${item.building.address}<br>
-        <strong>Città:</strong> ${item.building.citta.name}<br>
-        </div>
-        <button> Prenota </button>
-        </div>
-      `;
+          <div class="listaRis">
+          <div>
+          <strong>ID:</strong> ${item.id}<br>
+          <strong>Codice:</strong> ${item.codice}<br>
+          <strong>Descrizione:</strong> ${item.descrizione}<br>
+          <strong>Numero Max Occupanti:</strong> ${item.numeroMaxOccupanti}<br>
+          <strong>Disponibile:</strong> ${item.available ? "Sì" : "No"}<br>
+          <strong>Tipo:</strong> ${item.tipo}<br>
+          <strong>Edificio:</strong> ${item.building.name}<br>
+          <strong>Indirizzo:</strong> ${item.building.address}<br>
+          <strong>Città:</strong> ${item.building.citta.name}<br>
+          </div>
+          <button> Prenota </button>
+          </div>
+        `;
 
         // Aggiungi la riga al <li>
+        liElement.appendChild(row);
+
+        ulElement.appendChild(liElement);
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/* GET POSTAZIONI CON I CRITERI INDICATI DALL'UTENTE */
+
+async function getPostazioniDisponibili(citta, tipo) {
+  try {
+    tableElement.innerHTML = "";
+    ulElement.innerHTML = "";
+    let res = await fetch(
+      `http://localhost:8081/api/postazione/citta/${citta}/tipo/${tipo}`
+    );
+    console.log("Response status:", res.status);
+    if (res.ok) {
+      let data = await res.json();
+      console.log("data con parametri: citta e tipo", data);
+
+      /* NUOVO ELEMENTO TABELLA */
+
+      let tableElement = document.getElementById("risultatiLista");
+      if (data.content.length === 0) {
+        tableElement.style.display = "none";
+      } else {
+        tableElement.style.display = "table";
+
+        const theadElement = document.createElement("thead");
+
+        theadElement.innerHTML = `
+          <tr>
+             <th> <strong>Codice</strong> </th>
+             <th> <strong>Descrizione</strong> </th>
+             <th> <strong>N° Max</strong> </th>
+             <th> <strong>Disponibile</strong> </th>
+             <th> <strong>Tipo</strong> </th>
+             <th> <strong>Edificio</strong> </th>
+             <th> <strong>Indirizzo</strong> </th>
+             <th> <strong>Città</strong> </th>
+          </tr>
+          `;
+
+        tableElement.appendChild(theadElement);
+
+        data.content.forEach((item) => {
+          const trElement = document.createElement("tr");
+          trElement.innerHTML = `
+              <td>${item.codice}</td>
+              <td>${item.descrizione}</td>
+              <td>${item.numeroMaxOccupanti}</td>
+              <td>${item.available ? "Sì" : "No"}</td>
+              <td>${item.tipo}</td>
+              <td>${item.building.name}</td>
+              <td>${item.building.address}</td>
+              <td>${item.building.citta.name}</td>
+              <td> <button> Prenota </button></td>
+            `;
+
+          tableElement.appendChild(trElement);
+        });
+      }
+
+      /*  NUOVO ELEMENTO LISTA  */
+      const ulElement = document.getElementById("lista");
+
+      data.content.forEach((item) => {
+        const liElement = document.createElement("li");
+
+        const row = document.createElement("div");
+        row.innerHTML = `
+          <div class="listaRis">
+          <div>
+          <strong>ID:</strong> ${item.id}<br>
+          <strong>Codice:</strong> ${item.codice}<br>
+          <strong>Descrizione:</strong> ${item.descrizione}<br>
+          <strong>Numero Max Occupanti:</strong> ${item.numeroMaxOccupanti}<br>
+          <strong>Disponibile:</strong> ${item.available ? "Sì" : "No"}<br>
+          <strong>Tipo:</strong> ${item.tipo}<br>
+          <strong>Edificio:</strong> ${item.building.name}<br>
+          <strong>Indirizzo:</strong> ${item.building.address}<br>
+          <strong>Città:</strong> ${item.building.citta.name}<br>
+          </div>
+          <button> Prenota </button>
+          </div>
+        `;
+
         liElement.appendChild(row);
 
         ulElement.appendChild(liElement);
